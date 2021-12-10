fe6a1fb9dab201d0e39ce4e7eed8ca363010020d
Wofsauge <jan-.-@t-online.de>
2021-12-10 20:17:46 +0100

    Ported Buurazu's "IsaacXMLToLua" tool to python and added pocketitems to its tasks

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ local game = Game()
 require("eid_config")
 EID.Config = EID.UserConfig
 EID.Config.Version = "3.2"
-EID.ModVersion = "4.1"
+EID.ModVersion = "4.3"
 EID.DefaultConfig.Version = EID.Config.Version
 EID.isHidden = false
 EID.player = nil
