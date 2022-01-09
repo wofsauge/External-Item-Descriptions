ae164f9cb9a179fea67c59b50c9463c498d89e01
Potato <sjy3051@gmail.com>
2022-01-09 14:40:22 +0900

    Added integration font (hcrdotum+pixelmplus10) for Korean

--- descriptions/ab+/ko_kr.lua
+++ descriptions/ab+/ko_kr.lua
@@ -14,7 +14,7 @@ EID.descriptions[languageCode] = {}
 EID.descriptions[languageCode].custom = {} -- table for custom entity descriptions
 EID.descriptions[languageCode].languageName = "Korean"
 
-EID.descriptions[languageCode].fonts = {"korean_gulim", "korean_dotum"} -- Fonts to be used with this languagepack
+EID.descriptions[languageCode].fonts = {"korean_hcrdotum", "korean_gulim", "korean_dotum"} -- Fonts to be used with this languagepack
 
 EID.descriptions[languageCode].collectibles={
 	-- Original
