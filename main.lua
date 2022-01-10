ff325725a40a02cf178f7fc6196a96325d7319bb
Wofsauge <jan-.-@t-online.de>
2022-01-10 21:56:02 +0100

    Updated Workshop mod

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ local game = Game()
 require("eid_config")
 EID.Config = EID.UserConfig
 EID.Config.Version = "3.2" -- note: changing this will reset everyone's settings to default!
-EID.ModVersion = "4.10"
+EID.ModVersion = "4.11"
 EID.DefaultConfig.Version = EID.Config.Version
 EID.isHidden = false
 EID.player = nil
