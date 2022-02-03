e85bb42510b1f8be0083d4c51501a0e5f683b459
kittenchilly <mario.pikmin.zelda@gmail.com>
2022-02-03 08:36:54 -0600

    Prevent achievement helper trinket from spawning

--- main.lua
+++ main.lua
@@ -921,6 +921,14 @@ if Encyclopedia then
 	})
 end
 
+-- if the helper trinket somehow does spawn, replace it with a random trinket from the pool
+function EID:PreventHelperTrinketSpawn(entity)
+	if entity.SubType == achievementTrinket then
+		entity:Morph(entity.Type, entity.Variant, game:GetItemPool():GetTrinket())
+	end
+end
+EID:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, EID.PreventHelperTrinketSpawn, PickupVariant.PICKUP_TRINKET)
+
 local hasShownAchievementWarning = false
 local function renderAchievementInfo()
 	if REPENTANCE and not EID.Config.DisableAchievementCheck and game:GetFrameCount() < 10*30 then
