2246337fc4981face506c0dd4da1a6a532b2f911
kohashiwakaba <kanade.tinkle@gmail.com>
2022-04-11 11:16:41 +0900

    fix console errors on flipped items if front item is empty

--- eid_api.lua
+++ eid_api.lua
@@ -1587,7 +1587,7 @@ end
 
 -- Returns the quality of the described entity
 function EID:getObjectQuality(descObj)
-	if REPENTANCE and descObj.ObjType == 5 and descObj.ObjVariant == 100 then
+	if REPENTANCE and descObj.ObjType == 5 and descObj.ObjVariant == 100 and EID.itemConfig:GetCollectible(tonumber(descObj.ObjSubType)) then
 		return tonumber(EID.itemConfig:GetCollectible(tonumber(descObj.ObjSubType)).Quality)
 	end
 end
