0a487f964672e0355e1bd09c97af61fc5bec55b0
Buurazu <excel_saga_rules@hotmail.com>
2022-11-07 02:29:57 -0500

    More trinket effects

--- eid_data.lua
+++ eid_data.lua
@@ -723,9 +723,9 @@ EID.InlineColors = {
 -- append / findReplace / fullReplace: add text (Rainbow Worm, NO!) or entirely replace the trinket's description (Tick) from goldenTrinketEffects
 EID.GoldenTrinketData = {
 	-- Swallowed Penny, Petrified Poop (50% -> 60%), Purple Heart (max 2x), a couple Worms, Black Lipstick (max 2x), Bible Tract (max 2x), Monkey's Paw
-	[1] = 1, [2] = {t={50}, mult=1.2}, [5] = {t={2}, mult=2}, [10] = 0.4, [11] = 0.4, [17] = {t={1}, mult=2}, [18] = {t={3}, mult=2}, [20] = 1,
+	[1] = 1, [2] = {t={50}, mult=1.2}, [5] = {t={2}, mult=2}, [10] = {t={0.4,0}}, [11] = {t={0.4,0}}, [17] = {t={1}, mult=2}, [18] = {t={3}, mult=2}, [20] = 1,
 	-- Butt Penny, Hook Worm (two stats), Whip Worm, Fish Head, Liberty Cap (max 4x), Curved Horn, Goat Hoof, Mom's Pearl (2x becomes 3x)
-	[24] = {t={20}, mult=1.5}, [26] = {t={0.4, 1.5}}, [27] = 0.5, [29] = 1, [32] = {t={25}, mult=4}, [35] = 2, [37] = 0.15, [38] = {t={10}, mult=2},
+	[24] = {t={20}, mult=1.5}, [26] = {t={0.4, 1.5, 0}}, [27] = 0.5, [29] = 1, [32] = {t={25}, mult=4}, [35] = 2, [37] = 0.15, [38] = {t={10}, mult=2},
 	-- Cancer, Lucky Toe (just the Luck Up), Isaac's Fork (heart heal amount), A Missing Page (damage + necronomicon multiplier), Tick (the biggest pain)
 	[39] = 1, [42] = 1, [46] = {findReplace = true}, [48] = {t={2, 80},mults={1.5,2}}, [53] = {fullReplace = true},
 	-- Maggy's Faith, Rainbow Worm, Tape Worm, Lazy Worm, Louse, Watch Battery (just the charge amount), Exploding Cap, Stud Finder
@@ -744,8 +744,10 @@ EID.GoldenTrinketData = {
 	[176] = 1, [178] = {t={50},mult=2}, [183] = 0, [185] = 17, [186] = 1, [187] = {t={50,50}, mult=2},
 	
 	-- NEW REP PATCH UPDATE
-	-- AAA Battery, Broken Magnet (coins -> pickups), Flat Worm, Lucky Rock (effect chance rolls multiple times; default text), Mom's Toenail (20 -> 10 -> 6.66)
-	[3] = 1, [6] = {findReplace = true, mult = 2}, [12] = 50, [15] = 0, [16] = {t={20}, mults={0.5, 0.333}},
+	-- AAA Battery, Broken Magnet (coins -> pickups), Pulse Worm (default text), Flat Worm, Golden Store Credit,
+	[3] = 1, [6] = {findReplace = true, mult = 2}, [9] = 0, [12] = 50, [13] = {goldenOnly = true, fullReplace = true, mult=1},
+	-- Lucky Rock (effect chance rolls multiple times; default text), Mom's Toenail (20 -> 10 -> 6.66), Mysterious Candy (golden = golden poop)
+	[15] = 0, [16] = {t={20}, mults={0.5, 0.333}}, [25] = {goldenOnly = true, findReplace = true, mult = 2},
 }
 
 EID.BreakUtf8CharsLanguage = {
