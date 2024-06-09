124b87f8a7b4046820213d441181abb2584058b5
Buurazu <excel_saga_rules@hotmail.com>
2024-06-09 05:01:25 -0400

    Update eid_conditionals.lua

--- features/eid_conditionals.lua
+++ features/eid_conditionals.lua
@@ -13,17 +13,17 @@ EID.collectiblesToCheck = {}
 	Argument 2: The function that will apply the condition if it returns true
 	
 	Argument 3 (optional): Additional text for finding the string in the localization file
-	It will check for the string existing by itself, or appended to the ID
 	For example, if you put "Overridden" here, modifying Tech X, it will look for ConditionalDescs["5.100.395 (Overridden)"] AND ConditionalDescs["Overridden"]
 	
 	Argument 4 (optional): Table with any additional options, usually not needed:
-	abpOnly: Set to true if this condition shouldn't be ran in Repentance
-	repOnly: Set to true if this condition shouldn't be ran in Afterbirth+ (unnecessary if the string isn't in the AB+ localization file to begin with)
-	variableText: If there's a {1} in the desc, it will be replaced with this
-	locTable: Specify a different table to look for the localization string in (including the base table), rather than ConditionalDescs
+	variableText: If there's a {1} in the desc, it will be replaced with this; can be a function for more dynamic text results, it will get descObj passed into it
+	variableText2: If there's a {2} in the desc, it will be replaced with this; can be a function
+	locTable: Specify a different table to look for the localization string in, rather than ConditionalDescs
+	noTable: Set to true to look in the base localizations only (requires modText) (not used yet...)
 	lineColor: Appended lines will be highlighted with this color
 	replaceColor: Find/replace text will be highlighted with this color
 	bulletpoint: Appended lines will begin with this bulletpoint
+	noFallback: Don't fallback to English if this isn't localized; by default, conditionals don't fallback, to avoid printing English text in unupdated languages
 	
 	Argument 5 (very optional): The position in the table to insert this new condition, in case mods want to insert modifiers before our base ones
 ]]
@@ -35,6 +35,7 @@ function EID:AddConditional(IDs, funcText, modText, extraTable, insertPoint)
 	local newTable = {}; for k,v in pairs(extraTable) do newTable[k] = v end
 	newTable.func = funcText;
 	newTable.modifierText = modText;
+	if newTable.noFallback == nil then newTable.noFallback = true end
 	for _, id in ipairs(IDs) do
 		if type(id) ~= "string" then id = "5.100." .. id end
 		-- Check for only one period in the id string to mark it as being a general condition
@@ -46,108 +47,137 @@ function EID:AddConditional(IDs, funcText, modText, extraTable, insertPoint)
 	end
 end
 
--- These are easy to use functions for the most frequent conditionals: Having a specific collectible or having a specific player
--- They pass on the ID to the text, so the collectible/player icon/name can be inserted into the description if desired
--- This can also take a table for the owned IDs, so that for instance, Brimstone can easily have all of its "Overrides" assigned at once
--- checkInReminder is for if a synergy is no longer relevant once the item isn't obtainable (e.g. Abyss locusts)
+-- Easy to use functions for conditionals that rely on having a specific collectible/character
+-- By default, they'll have a bulletpoint for the coll/char, and {1} becomes the coll/char's name
+
+-- checkInReminder is for if a synergy is no longer relevant once the item isn't obtainable (e.g. Abyss locusts) (true by default)
 function EID:AddCollectibleConditional(IDs, ownedIDs, modText, extraTable, checkInReminder)
 	if type(ownedIDs) ~= "table" then ownedIDs = {ownedIDs} end
 	if checkInReminder == nil then checkInReminder = true end
 	extraTable = extraTable or {}
 	for _, ownedID in ipairs(ownedIDs) do
 		EID.collectiblesToCheck[ownedID] = true
-		extraTable.variableText = "{{NameC" .. ownedID .. "}}";
+		if extraTable.variableText == nil then extraTable.variableText = "{{NameOnlyC" .. ownedID .. "}}" end
+		if extraTable.bulletpoint == nil then extraTable.bulletpoint = "Collectible" .. ownedID end
 		EID:AddConditional(IDs, function() return EID:ConditionalCollCheck(ownedID,checkInReminder) end, modText, extraTable)
 	end
 end
 
--- For adding pairs of abilities to collectibles; you can define a different modifier text for them
--- example: EID:AddSynergyConditional({list of things overridden by Brimstone}, Brimstone, "Overridden", "Overrides")
+-- For adding pairs of abilities to collectibles; you can define a different modifier text for each of them
+-- example: EID:AddSynergyConditional({list of IDs overridden by Brimstone}, Brimstone, "Overridden", "Overrides")
 function EID:AddSynergyConditional(IDs, ownedIDs, modText1, modText2, extraTable1, extraTable2, checkInReminder)
 	EID:AddCollectibleConditional(IDs, ownedIDs, modText1, extraTable1, checkInReminder)
 	EID:AddCollectibleConditional(ownedIDs, IDs, modText2 or modText1, extraTable2 or extraTable1, checkInReminder)
 end
 
--- By default, this checks for having the regular or tainted character, as all our conditionals so far apply to both
--- There is a fifth argument for it, or use AddNonTaintedPlayerConditional
--- This can also take in a table for the character IDs, most useful for two-part chars like Forgotten or Lazarus
+-- includeTainted is for if you want to check the normal and tainted version of a character (true by default)
 function EID:AddPlayerConditional(IDs, charIDs, modText, extraTable, includeTainted)
 	if type(charIDs) ~= "table" then charIDs = {charIDs} end
 	if includeTainted == nil then includeTainted = true end
 	extraTable = extraTable or {}
 	for _, charID in ipairs(charIDs) do
-		extraTable.variableText = "{{NameI" .. charID .. "}}";
+		if extraTable.variableText == nil then extraTable.variableText = "{{NameOnlyI" .. charID .. "}}" end
+		if extraTable.bulletpoint == nil then extraTable.bulletpoint = "Player" .. charID end
 		EID:AddConditional(IDs, function() return EID:ConditionalCharCheck(charID,includeTainted) end, modText, extraTable)
 	end
 end
-function EID:AddNonTaintedPlayerConditional(IDs, charID, modText, extraTable)
-	AddPlayerConditional(IDs, charID, modText, extraTable, false)
-end
 
--- Adding our conditionals
--- TODO: add a debugging command that tells you about missing expected strings / when two conditions share a string they shouldn't?
--- new config options for spoiler-heavy descriptions if we make any (chaos card and plan C are important)
+-- Adding our conditionals (should this be its own file away from the functions that add/print conditionals?)
+
 --[[
-Descs that need a variant (I made this list months ago):
-	things that apply only to Lost/Tainted Lost (broken heart card?) (Heartbreak?)
-	spoilers descs
-	lachyphragy in co-op or is that obvious? tech zero / extension cord in co-op? yum heart and such
-	black candle + cursed penny / cursed eye
-	healing stuff for keeper? (what, what did this sentence mean, the flies? who cares)
-	Items that have synergies with other specific items (duh)
-	Items that override other items (brimstone, mom's knife, epic fetus stuff)
-	BFFs / the fly booster + Tainted Apollyon, Tainted Eve; melody trinket + Tainted Eve
+TODO STUFF:
+
+new config options for spoiler-heavy descriptions if we make any (chaos card and plan C?)
+CO-OP BENEFIT ITEMS! tech zero / extension cord in co-op? yum heart and such
+Items that override other items (brimstone, mom's knife, epic fetus stuff)
+BFFs / the fly booster + Tainted Apollyon, Tainted Eve; melody trinket + Tainted Eve; these are insane synergies that deserve attention
+TODO: Make some of these coll/char IDs be from the enum for readability? or just keep it to comments
+TODO: A find/replace pair for some Keeper descriptions that talk about Red hearts but work for him, Red -> Coin or Red/Coin; Could be good for Dead Cat
+TODO: AB+ tarot cloth buffs table?
+-- hey what if all these Luck addons that I kinda don't like instead showed an accurate percentage of what they do at your current luck (although this is Modifier territory, with a per character display)
+-- todo: TrinketConditional, use it for the crystal key + pandora's box or whichever key it is, and Black Feather items maybe
+-- todo: Vibrant/Dim Bulb + doesn't work with pocket consumables. could be conditional actually if we have a list of char IDs with pocket actives
+-- todo: idk about the BFFs synergy stuff but I noticed that Brown Nugget is incorrectly dealt with, it gained double damage in rep
+-- hive mind and BFFs synergy has a lot of overlap also
+-- brown nugget + sacrificial altar?!
+-- ? Card + single use actives in Repentance? Alabaster Box
+
+-- meta todo: Put a 100% save file for AB+ and for Rep on my flash drive
+
 ]]
 
--- General conditions (used to be in eid_modifiers)
--- Tarot Cloth + Cards
-EID:AddCollectibleConditional("5.300", 451, nil, {locTable = "tarotClothBuffs", replaceColor = "ColorShinyPurple", bulletpoint = "Collectible451"})
--- Abyss (doesn't apply in item reminder)
-EID:AddCollectibleConditional("5.100", 706, nil, {locTable = "abyssSynergies", lineColor = "ColorRed", bulletpoint = "Collectible706"}, false)
--- Book of Belial Birthright
-EID:AddCollectibleConditional("5.100", 59, nil, {locTable = "bookOfBelialBuffs", bulletpoint = "Collectible34"})
--- Binge Eater
-EID:AddCollectibleConditional("5.100", 664, nil, {locTable = "bingeEaterBuffs", bulletpoint = "Collectible664"})
--- Car Battery
-EID:AddCollectibleConditional("5.100", 356, nil, {locTable = "carBattery", replaceColor = "BlinkYellowGreen", bulletpoint = "Collectible356"})
-
--- Greed Mode
-EID:AddConditional({241, "5.300.15"}, function() return Game():IsGreedMode() end) -- Contract From Below, Temperance
-EID:AddConditional("5.300.15", function() return Game():IsGreedMode() and EID:PlayersHaveCollectible(451) end, "Tarot") -- Temperance + Greed + Tarot
+-- General conditions
+-- Tarot Cloth (AB+ and Rep) (fallback to English because most are just find+replace)
+EID:AddCollectibleConditional("5.300", 451, nil, {locTable = "tarotClothBuffsAB", replaceColor = "ColorShinyPurple", noFallback = false})
+if EID.isRepentance then EID:AddCollectibleConditional("5.300", 451, nil, {locTable = "tarotClothBuffs", replaceColor = "ColorShinyPurple", noFallback = false}) end
+EID:AddCollectibleConditional("5.100", 356, nil, {locTable = "carBattery", replaceColor = "BlinkYellowGreen"}) -- Car Battery
 
 -- IV Bag conditions (need to be applied in a specific order)
 EID:AddCollectibleConditional(135, 75, "PHD") -- PHD improves IV Bag
-EID:AddPlayerConditional(135, 14, "Keeper") -- Keeper gets 0-1 coins
-EID:AddConditional(135, function() return EID:IsHardMode() end, "Hard Mode") -- Hard Mode pays out less
+EID:AddPlayerConditional(135, 14, "Keeper 0-1") -- Keeper gets 0-1 coins
+if EID.isRepentance then EID:AddConditional(135, function() return EID:IsHardMode() end, "Hard Mode") end -- Hard Mode pays out less
+
+-- Greed Mode
+EID:AddConditional({241, "5.300.15"}, function() return game:IsGreedMode() end) -- Contract From Below, Temperance
+EID:AddConditional("5.300.15", function() return game:IsGreedMode() and EID:PlayersHaveCollectible(451) end, "Tarot") -- Temperance + Greed + Tarot
 
 -- Different effect for No Red Health players
+-- TODO: separate this into a Rep-only part, and figure out what they do exactly with Keeper/Forgotten
 EID:AddConditional({81, 442, 676}, function() return EID:CheckForNoRedHealthPlayer() end, "No Red") -- Dead Cat, Dark Prince's Crown, Empty Heart
---TODO: Keeper/Forgotten DON'T benefit from Empty Heart but DO benefit from Dark Prince's Crown? Nothing's consistent...
+--TODO: Keeper/Forgotten DON'T benefit from Empty Heart but DO benefit from Dark Prince's Crown? Nothing's consistent... does the Soul benefit while Forgotten does
 
 -- Achievement checks (these always return true without REPENTOGON)
 EID:AddConditional("5.350.23", function() return EID:HaveNotUnlockedAchievement(82) end) -- Sacrifice Poster unlocks The Lost
 
--- Tainted characters reviving as themselves
-EID:AddPlayerConditional({161, "5.350.28"}, 25, "Tainted Revive") -- Ankh, Broken Ankh
-EID:AddPlayerConditional(311, 24, "Tainted Revive") -- Judas's Shadow
-EID:AddPlayerConditional(332, {29, 38}, "Tainted Revive") -- Lazarus's Rags
-EID:AddPlayerConditional("5.350.23", 31, "Tainted Revive") -- Sacrifice Poster
-
 -- Specific character synergies/changes
-EID:AddPlayerConditional({240, 644}, 21, "Tainted Isaac") -- Tainted Isaac + Experimental Treatment, Consolation Prize
 EID:AddPlayerConditional(549, {10,14})-- Keeper/Lost + Brittle Bones
-EID:AddPlayerConditional(642, 10) -- Lost + Magic Skin
-EID:AddPlayerConditional(694, 10) -- Lost + Heartbreak
-EID:AddPlayerConditional("5.350.1", 14) -- Keeper + Swallowed Penny
+EID:AddPlayerConditional(108, 14, "No Effect") -- Keeper + The Wafer
+EID:AddPlayerConditional(227, 14, "Keeper 0-1") -- Keeper + Piggy Bank
+EID:AddPlayerConditional(501, 14) -- Keeper + Greed's Gullet
+EID:AddPlayerConditional(230, 14, "Keeper") -- Keeper + Abaddon
 
 -- Item Synergies
-EID:AddCollectibleConditional(201, 147) -- Iron Bar refills Notched Axe
 EID:AddSynergyConditional(7, 34); EID:AddCollectibleConditional("5.300.16", 7) -- Martyr + Book of Belial/The Devil
-EID:AddSynergyConditional(316, 260); EID:AddCollectibleConditional("5.350.172", 260) -- Black Candle + Cursed Eye/Cursed Penny
+EID:AddSynergyConditional(316, 260) -- Black Candle + Cursed Eye
+EID:AddCollectibleConditional("5.300.48", 286, nil, {lineColor = "ColorSilver"}) -- Blank Card + ? Card
 
 -- Overridden by Brimstone
 -- Haven't done it yet but I'm sure preparing the system for it
+-- Remember most of these will be AB+ only (not EID.isRepentance)
 
+
+if EID.isRepentance then
+	-- General conditions
+	EID:AddCollectibleConditional("5.100", 706, nil, {locTable = "abyssSynergies", lineColor = "ColorRed"}, false) -- Abyss (no item reminder)
+	EID:AddCollectibleConditional("5.100", 59, nil, {locTable = "bookOfBelialBuffs"}) -- Belial Birthright
+	EID:AddCollectibleConditional("5.100", 664, nil, {locTable = "bingeEaterBuffs"}) -- Binge Eater
+	
+	-- Co-op friendly items
+	-- todo (yum heart, extension cord)
+	
+	-- Tainted characters reviving as themselves
+	EID:AddPlayerConditional({161, "5.350.28"}, 25, "Tainted Revive") -- Ankh, Broken Ankh
+	EID:AddPlayerConditional(311, 24, "Tainted Revive") -- Judas's Shadow
+	EID:AddPlayerConditional(332, {29, 38}, "Tainted Revive") -- Lazarus's Rags
+	EID:AddPlayerConditional("5.350.23", 31, "Tainted Revive") -- Sacrifice Poster
+
+	-- Specific character synergies/changes
+	EID:AddPlayerConditional("5.350.1", 14, "Keeper 0-1") -- Keeper + Swallowed Penny
+	EID:AddPlayerConditional(188, 2) -- Cain + Abel
+	EID:AddPlayerConditional(360, 13) -- Incubus + Lilith
+	EID:AddPlayerConditional({240, 644}, 21) -- Tainted Isaac + Experimental Treatment, Consolation Prize
+	EID:AddPlayerConditional({642, 694}, 10) -- Lost + Magic Skin, Heartbreak
+	EID:AddPlayerConditional("5.350.156", 14) -- Keeper + Mother's Kiss
+	EID:AddPlayerConditional(230, 18, "Bethany", nil, false) -- Bethany + Abaddon
+	EID:AddPlayerConditional(230, 36, "Tainted Bethany") -- Tainted Bethany + Abaddon
+	EID:AddPlayerConditional(245, 14, "Keeper") -- 20/20 + Keeper
+	
+	-- Item Synergies
+	EID:AddCollectibleConditional(201, 147) -- Iron Bar refills Notched Axe
+	EID:AddCollectibleConditional("5.350.172", 260) -- Black Candle + Cursed Penny
+	EID:AddCollectibleConditional(501, 416) -- Greed's Gullet + Deep Pockets
+	EID:AddSynergyConditional(245, {2, 153, 169}, nil, "20/20") -- 20/20 + Mutant Spider, The Inner Eye, Polyphemus
+end
 
 ----- Evaluation Functions -----
 
@@ -156,7 +186,7 @@ function EID:ConditionalCharCheck(playerType, includeTainted)
 	if EID.InsideItemReminder then
 		local player = EID:ItemReminderGetAllPlayers()[EID.ItemReminderSelectedPlayer + 1] or EID.player
 		if player:GetPlayerType() == playerType then return true
-		elseif includeTainted and REPENTANCE and Isaac.GetPlayerTypeByName(player:GetName()) == playerType then return true
+		elseif includeTainted and EID.isRepentance and Isaac.GetPlayerTypeByName(player:GetName()) == playerType then return true
 		else return false end
 	else
 		return EID:PlayersHaveCharacter(playerType, includeTainted)
@@ -252,7 +282,7 @@ function EID:applyConditionals(descObj)
 	EID:CheckPlayersCollectibles()
 	
 	local typeVar = descObj.ObjType.."."..descObj.ObjVariant -- for general conditions (Tarot Cloth, Book of Virtues)
-	local typeVarSub = descObj.ObjType.."."..descObj.ObjVariant.."."..descObj.ObjSubType -- for specific conditions
+	local typeVarSub = descObj.fullItemString -- for specific conditions
 	-- Combine general+specific conditions into one table
 	local conds = {}
 	if EID.DescriptionConditions[typeVar] then TableConcat(conds, EID.DescriptionConditions[typeVar]) end
@@ -262,20 +292,27 @@ function EID:applyConditionals(descObj)
 		-- Search for the localization string; "S" (for generals) or "T.V.S" or "T.V.S (Modifier)" or "Modifier"
 		local locTable = cond.locTable or "ConditionalDescs"
 		local text = nil
-		if cond.general then
-			text = EID:getDescriptionEntry(locTable, descObj.ObjSubType, true)
+		
+		if cond.noTable then
+			text = EID:getDescriptionEntry(cond.modifierText, nil, cond.noFallback)
+		elseif cond.general then
+			text = EID:getDescriptionEntry(locTable, descObj.ObjSubType, cond.noFallback)
 		elseif cond.modifierText then
-			text = EID:getDescriptionEntry(locTable, typeVarSub .. " (" .. cond.modifierText .. ")", true)
-			if text == nil then text = EID:getDescriptionEntry(locTable, cond.modifierText, true) end
+			text = EID:getDescriptionEntry(locTable, typeVarSub .. " (" .. cond.modifierText .. ")", cond.noFallback)
+			if text == nil then text = EID:getDescriptionEntry(locTable, cond.modifierText, cond.noFallback) end
 		else
-			text = EID:getDescriptionEntry(locTable, typeVarSub, true)
+			text = EID:getDescriptionEntry(locTable, typeVarSub, cond.noFallback)
 		end
 		
 		-- If we find the localization string for this condition, and it passes the test, modify the description text
-		if text ~= nil and (not cond.repOnly or REPENTANCE) and (not cond.abpOnly or not REPENTANCE) and cond.func() then
+		if text ~= nil and cond.func() then
+			local variableText = type(cond.variableText) == "function" and cond.variableText(descObj) or cond.variableText
+			local variableText2 = type(cond.variableText2) == "function" and cond.variableText2(descObj) or cond.variableText2
+			
 			-- String = append
 			if type(text) == "string" then
-				text = EID:ReplaceVariableStr(text, 1, cond.variableText)
+				text = EID:ReplaceVariableStr(text, 1, variableText)
+				text = EID:ReplaceVariableStr(text, 2, variableText2)
 				local iconStr = "#"
 				if cond.bulletpoint then iconStr = iconStr .. "{{" .. cond.bulletpoint .. "}} " end
 				if cond.lineColor then iconStr = iconStr .. "{{" .. cond.lineColor .. "}}" end
@@ -283,15 +320,16 @@ function EID:applyConditionals(descObj)
 				
 			-- Table with 1 entry = replace
 			elseif #text == 1 then
-				descObj.Description = EID:ReplaceVariableStr(text[1], 1, cond.variableText)
+				descObj.Description = EID:ReplaceVariableStr(EID:ReplaceVariableStr(text[1], 1, variableText), 2, variableText2)
 				
 			-- Table with 2+ entries = find and replace pairs
 			-- Entry 1 is replaced with entry 2, entry 3 is replaced with entry 4, etc.
 			else
 				local pos = 1
 				while pos < #text do
-					local toFind = EID:ReplaceVariableStr(text[pos], 1, cond.variableText)
-					local replaceWith = EID:ReplaceVariableStr(text[pos+1], 1, cond.variableText)
+					local toFind = text[pos]
+					local replaceWith = EID:ReplaceVariableStr(text[pos+1], 1, variableText)
+					replaceWith = EID:ReplaceVariableStr(replaceWith, 2, variableText2)
 					if cond.replaceColor then replaceWith = "{{" .. cond.replaceColor .. "}}" .. replaceWith .. "{{CR}}" end
 					--"%d*%.?%d+" will grab every number group (1, 10, 0.5), this will allow us to not replace the "1" in "10" erroneously
 					if (type(toFind) == "number") then
