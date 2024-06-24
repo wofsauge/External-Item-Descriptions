cbf0c70fa8cf04602c99164452d76731eea805c1
Buurazu <excel_saga_rules@hotmail.com>
2024-06-24 07:42:00 -0400

    tech 2 conditional

--- descriptions/ab+/en_us.lua
+++ descriptions/ab+/en_us.lua
@@ -1257,6 +1257,7 @@ EID.descriptions[languageCode].ConditionalDescs = {
 	["5.70.28"] = "Isaac shoots forward and to the sides instead", -- R U A Wizard + The Wiz
 	["5.100.523"] = "Counts as a passive item to {1}", -- Moving Box + Void
 	["Mongo Babies"] = "Can be copied by {1}", -- Mongo Baby + Baby Familiars
+	["Technology 2 One Eye"] = "With {1}, the laser replaces your tears entirely", -- Technology 2 + one eye
 }
 
 
