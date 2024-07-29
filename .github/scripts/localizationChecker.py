b74cf0e225dfa5fb0cc0502abe1405bca8ae1a0d
Wofsauge <jan-.-@t-online.de>
2024-07-30 00:40:51 +0200

    fixed CI and improved end result formatting

--- .github/scripts/localizationChecker.py
+++ .github/scripts/localizationChecker.py
@@ -3,7 +3,7 @@ from lupa.lua54 import LuaRuntime
 lua = LuaRuntime(unpack_returned_tuples=True)
 import glob, os
 SCRIPT_PATH = os.path.realpath(__file__)
-SOURCE_MOD_DIRECTORY = os.path.dirname(SCRIPT_PATH)
+SOURCE_MOD_DIRECTORY = os.path.dirname(SCRIPT_PATH)+"\\..\\.."
 
 if "GITHUB_WORKSPACE" in os.environ:
     SOURCE_MOD_DIRECTORY = os.environ["GITHUB_WORKSPACE"]
@@ -20,13 +20,14 @@ Blue='\033[0;34m'         # Blue
 Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
+BWhite='\033[1;37m'       # Bold White
 
 
 lua.execute('EID = {}; EID.descriptions = {} function EID:updateDescriptionsViaTable(changeTable, tableToUpdate) for k,v in pairs(changeTable) do if v == "" then tableToUpdate[k] = nil else tableToUpdate[k] = v end	end end')
 
 print("reading: en_us.lua",)
-lua.execute(open(SOURCE_MOD_DIRECTORY+"\\..\\..\\descriptions\\ab+\\en_us.lua", "r", encoding="UTF-8").read())
-lua.execute(open(SOURCE_MOD_DIRECTORY+"\\..\\..\\descriptions\\rep\\en_us.lua", "r", encoding="UTF-8").read())
+lua.execute(open(SOURCE_MOD_DIRECTORY+"\\descriptions\\ab+\\en_us.lua", "r", encoding="UTF-8").read())
+lua.execute(open(SOURCE_MOD_DIRECTORY+"\\descriptions\\rep\\en_us.lua", "r", encoding="UTF-8").read())
 
 g = lua.globals()
 # count en_us entries for stats
@@ -61,7 +62,7 @@ en_us_entries = count_entries(g.EID['descriptions']['en_us'])
 print("en_us entries:", en_us_entries)
 
 languages = {}
-for file in glob.glob(".\\descriptions\\ab+\\*.lua") + glob.glob(".\\descriptions\\rep\\*.lua"):
+for file in glob.glob(SOURCE_MOD_DIRECTORY+"\\descriptions\\ab+\\*.lua") + glob.glob(SOURCE_MOD_DIRECTORY+"\\descriptions\\rep\\*.lua"):
     if "en_us" not in file and "transformations" not in file:
         print("reading:",file)
         lua.execute(open(file, "r", encoding="UTF-8").read())
@@ -71,11 +72,10 @@ for lang in languages:
     print(f"Evaluating language '{lang}'..")
     progress = [0, 0]
     compare_tables(g.EID['descriptions']['en_us'], g.EID['descriptions'][lang], lang, progress)
-    errors = en_us_entries - progress[1]
     percentage = (en_us_entries-progress[1])/en_us_entries*100
-    languages[lang] = percentage
-    print(f"{Red}Errors found: {progress[1]} / {en_us_entries}{Color_Off}")
+    languages[lang] = [percentage, progress[1]]
+    print(f"{Red}Errors found: {progress[1]} / {en_us_entries}{Color_Off}\n")
 
 print(f"{Blue}Translation progress:{Color_Off}")
 for lang in languages:
-    print(f"\t{Blue}{lang}:\t{languages[lang]}%{Color_Off}")
+    print(f"\t{BWhite}{lang}{Color_Off}\t{Blue}{languages[lang][0]}%{Color_Off}\t{Red}{languages[lang][1]} missing{Color_Off}")
