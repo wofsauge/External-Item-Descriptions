1dbd571830d16117a096a7c1014fc400cba4c977
Wofsauge <jan-.-@t-online.de>
2024-07-30 21:19:28 +0200

    hopefully fixed ci

--- .github/scripts/localizationChecker.py
+++ .github/scripts/localizationChecker.py
@@ -2,6 +2,11 @@ import lupa.lua54 as lupa
 from lupa.lua54 import LuaRuntime
 lua = LuaRuntime(unpack_returned_tuples=True)
 import glob, os
+SCRIPT_PATH = os.path.realpath(__file__)
+SOURCE_MOD_DIRECTORY = os.path.dirname(SCRIPT_PATH)+"/../.."
+
+if "GITHUB_WORKSPACE" in os.environ:
+    SOURCE_MOD_DIRECTORY = os.environ["GITHUB_WORKSPACE"]
 
 # Reset
 Color_Off='\033[0m'       # Text Reset
@@ -17,14 +22,9 @@ Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
 BWhite='\033[1;37m'       # Bold White
 
+ignoreNodesWithName = {"fonts"}
 
-lua.execute('EID = {}; EID.descriptions = {} function EID:updateDescriptionsViaTable(changeTable, tableToUpdate) for k,v in pairs(changeTable) do if v == "" then tableToUpdate[k] = nil else tableToUpdate[k] = v end	end end')
 
-print("reading: en_us.lua",)
-lua.execute(open(".\\descriptions\\ab+\\en_us.lua", "r", encoding="UTF-8").read())
-lua.execute(open(".\\descriptions\\rep\\en_us.lua", "r", encoding="UTF-8").read())
-
-g = lua.globals()
 # count en_us entries for stats
 def count_entries(t):
     count = 0
@@ -34,30 +34,41 @@ def count_entries(t):
             count += count_entries(t[k])
     return count
 
-def compare_tables(table1, table2, prev_key, progress):
+# find entries in table2 that are missing or different than in table 1
+def compare_tables(table1, table2, prev_key):
+    errorCount = 0
     for k in table1:
-        progress[0] += 1
+        if k in ignoreNodesWithName:
+            # dont evaluate nodes that are listed in this table
+            continue
         if k not in table2:
             print(f"\tTable '{prev_key}' does not contain key: {k}")
-            progress[1] += 1
+            errorCount += 1
             # table is missing. add all missing entries as error
             if lupa.lua_type(table1[k]) == "table": 
-                progress[1] += count_entries(table1[k])
+                errorCount += count_entries(table1[k])
         elif lupa.lua_type(table2[k]) != lupa.lua_type(table1[k]):
             print(f"\tType mismatch in table '{prev_key}', key: {k}")
-            progress[1] += 1
+            errorCount += 1
         elif lupa.lua_type(table2[k]) == "table":
-            compare_tables(table1[k], table2[k], str(prev_key)+"->"+str(k), progress)
+            errorCount += compare_tables(table1[k], table2[k], str(prev_key)+"->"+str(k))
+    return errorCount
 
 
+lua.execute('EID = {}; EID.descriptions = {} function EID:updateDescriptionsViaTable(changeTable, tableToUpdate) for k,v in pairs(changeTable) do if v == "" then tableToUpdate[k] = nil else tableToUpdate[k] = v end	end end')
+g = lua.globals()
 
 
+print("reading: en_us.lua")
+for englishFile in glob.glob(SOURCE_MOD_DIRECTORY+"/**/en_us.lua", recursive=True):
+    lua.execute(open(englishFile, "r", encoding="UTF-8").read())
 
 en_us_entries = count_entries(g.EID['descriptions']['en_us'])
 print("en_us entries:", en_us_entries)
 
+
 languages = {}
-for file in glob.glob(".\\descriptions\\ab+\\*.lua") + glob.glob(".\\descriptions\\rep\\*.lua"):
+for file in glob.glob(SOURCE_MOD_DIRECTORY+"/**/ab+/*.lua", recursive=True) + glob.glob(SOURCE_MOD_DIRECTORY+"/**/rep/*.lua", recursive=True):
     if "en_us" not in file and "transformations" not in file:
         print("reading:",file)
         lua.execute(open(file, "r", encoding="UTF-8").read())
@@ -65,12 +76,13 @@ for file in glob.glob(".\\descriptions\\ab+\\*.lua") + glob.glob(".\\description
 
 for lang in languages:
     print(f"Evaluating language '{lang}'..")
-    progress = [0, 0]
-    compare_tables(g.EID['descriptions']['en_us'], g.EID['descriptions'][lang], lang, progress)
-    percentage = (en_us_entries-progress[1])/en_us_entries*100
-    languages[lang] = [percentage, progress[1]]
-    print(f"{Red}Errors found: {progress[1]} / {en_us_entries}{Color_Off}\n")
+    errorCount = compare_tables(g.EID['descriptions']['en_us'], g.EID['descriptions'][lang], lang)
+    percentage = (en_us_entries - errorCount) / en_us_entries*100
+    languages[lang] = [percentage, errorCount]
+    print(f"{Red}Errors found: {errorCount} / {en_us_entries}{Color_Off}\n\n")
 
 print(f"{Blue}Translation progress:{Color_Off}")
 for lang in languages:
-    print(f"\t{BWhite}{lang}{Color_Off}\t{Blue}{round(languages[lang][0],2)}%{Color_Off}\t{Red}{languages[lang][1]} missing{Color_Off}")
+    print(
+        f"\t{BWhite}{lang}{Color_Off}\t{Blue}{round(languages[lang][0],2)}%{Color_Off}\t{Red}{languages[lang][1]} missing{Color_Off}"
+    )
