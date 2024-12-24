8714fcfc29f31bf0b84c77dd8a5c326e41a4ea26
f0lk-amy <falkeduardstefan@gmail.com>
2024-12-24 19:31:35 +0200

    Update ro.lua

--- descriptions/rep/ro.lua
+++ descriptions/rep/ro.lua
@@ -8,7 +8,7 @@
 -- ↑ = Up Arrow  |  ↓ = Down Arrow  |  # = Starts a new line
 -- More can be found here: https://github.com/wofsauge/External-Item-Descriptions/wiki/Markup
 
-local languageCode = "RO_ro"
+local languageCode = "ro_ro"
 
 ---------- Collectibles ----------
 
