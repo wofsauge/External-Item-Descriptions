7eb05c7f6bcdf7891773513f52e82796d6aa5b34
f0lk-amy <falkeduardstefan@gmail.com>
2025-02-08 02:18:22 +0200

    Update and rename ro.lua to ro_ro.lua

--- descriptions/ab+/ro_ro.lua
+++ descriptions/ab+/ro_ro.lua
@@ -8,7 +8,7 @@
 -- ↑ = Up Arrow  |  ↓ = Down Arrow  |  # = Starts a new line
 -- More can be found here: https://github.com/wofsauge/External-Item-Descriptions/wiki/Markup
 
-local languageCode = "ro"
+local languageCode = "ro_ro"
 
 -- init en_us table
 EID.descriptions[languageCode] = {}
