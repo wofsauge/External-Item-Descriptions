65f8b94350880e936f3daaaa4a761202d00b3c3c
Wofsauge <jan-.-@t-online.de>
2025-02-22 22:55:26 +0100

    Updated Czech translation with 1.9.7.10 changes

--- descriptions/rep+/cs_cz.lua
+++ descriptions/rep+/cs_cz.lua
@@ -35,6 +35,8 @@ local collectibles = {
 	[594] = {"594", "Jupiter", "↑ {{EmptyHeart}} +2 prázdná místa pro srdce#↓ {{Speed}} Snížení rychlosti o -0.3#{{HealingRed}} Vyléčí 2 srdce#{{Speed}} Zvýšení až +0,5 rychlosti, pokud stojíš na místě#{{Poison}} Při pohybu se za tebou uvolňují jedovaté prdy#{{Poison}} Imunita vůči jedu"}, -- Jupiter
 	-- Change: Complete rewrite
 	[632] = {"632", "Zlé Kouzlo", "↑ {{Luck}} +2 Štěstí#Imunita vůči: {{Burning}} ohni, {{Confusion}} zmatení, {{Fear}} strachu, {{Slow}} pavučin a {{Poison}} jedovatého efektu#Poskytuje 1 sekundu imunity proti kalužím"}, -- Evil Charm
+	-- Change: Added Shop as a new destination
+	[660] = {"660", "Čtení z Karet", "Vytvoří dva portály na začátku každého patra#Portály zmizí, když opustíš místnost#{{Blank}} {{ColorRed}}Červený: {{CR}}{{BossRoom}} Boss místnost#{{Blank}} {{ColorYellow}}Žlutý: {{CR}}{{TreasureRoom}} Pokladnice#{{Blank}} {{ColorBlue}}Modrý: {{CR}}{{SecretRoom}} Tajná místnost#{{Blank}} {{ColorGreen}}Zelená: {{CR}}{{Shop}} Obchod"}, -- Card Reading
 	-- Change: Complete rewrite
 	[681] = {"681", "Malý Portál", "Dvojitým klepnutím tlačítkem střelby vymrštíš před sebou Malý Portál#Způsobuje kontaktní poškození když letí dopředu#Konzumuje při tom pickupy, které má před sebou#Každý z nich mu zvětší jeho velikost, poškození a vytvoří modrou mušku#Po 2-3 pickupech vytvoří portál do speciální místnosti a spojenec zmizí po zbytek patra#Obsah v této místnosti přetrvává po zbytek běhu"}, -- Lil Portal
 	-- Change: Added info about the damage based on item quality
@@ -57,6 +59,15 @@ local trinkets = {
 	[105] = {"105", "Oběd v Sáčku", "{{Collectible22}} 5% šance na vytvoření oběda, když dostaneš zásah"}, -- Bag Lunch
 }
 EID:updateDescriptionsViaTable(trinkets, EID.descriptions[languageCode].trinkets)
+
+---------- Cards ----------
+
+local cards = {
+	-- Change: Complete rewrite
+	[38] = {"38", "Berkano", "{{Collectible706}} Vytvoří 3 mušky Hlubiny pro aktuální místnost"}, -- Berkano
+}
+EID:updateDescriptionsViaTable(cards, EID.descriptions[languageCode].cards)
+
 
 ---------- Conditions ----------
 EID.descriptions[languageCode].ConditionalDescs["5.100.566"] = nil -- Dream Catcher (Greed) - In Rep+, the dream preview works in greed mode as well, so no changes needed
