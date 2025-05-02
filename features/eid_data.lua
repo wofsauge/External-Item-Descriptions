e774aa9186e55da3a33bf982fdf26662949b4938
kittenchilly <mario.pikmin.zelda@gmail.com>
2025-05-02 13:27:35 -0500

    Use ItemConfig for more stuff that was done by the LUT (#838)
    * Use ItemConfig for more stuff that was done by the LUT
    
    * Missed some stuff
    
    * Update lookuptable_generator.py

--- features/eid_data.lua
+++ features/eid_data.lua
@@ -1087,11 +1087,6 @@ EID.HealthTypesWithoutHealing["None"] = true
 -- Character IDs that have a pocket active (0 = normal, 1 = timed, 2 = special)
 EID.PocketActivePlayerIDs = { [22] = 0, [23] = 2, [24] = 1, [25] = 2, [26] = 1, [29] = 0, [34] = 0, [36] = 0, [37] = 1, [38] = 0, [39] = 1 }
 
--- Cards that don't work with Blank Card in Repentance (Note: ? Card is blacklisted here, don't use this for determining what is a card)
-EID.blankCardHidden = {[32]=true,[33]=true,[34]=true,[35]=true,[36]=true,[37]=true,[38]=true,[39]=true,[40]=true,[41]=true,[48]=true,[49]=true,[50]=true,[55]=true,[78]=true,[81]=true,[82]=true,[83]=true,[84]=true,[85]=true,[86]=true,[87]=true,[88]=true,[89]=true,[90]=true,[91]=true,[92]=true,[93]=true,[94]=true,[95]=true,[96]=true,[97]=true,}
--- Cards that are treated as runes
-EID.runeIDs = {[32]=true,[33]=true,[34]=true,[35]=true,[36]=true,[37]=true,[38]=true,[39]=true,[40]=true,[41]=true,[55]=true,[81]=true,[82]=true,[83]=true,[84]=true,[85]=true,[86]=true,[87]=true,[88]=true,[89]=true,[90]=true,[91]=true,[92]=true,[93]=true,[94]=true,[95]=true,[96]=true,[97]=true,}
-
 -- "Evil" item IDs for Black Feather
 EID.blackFeatherItems = {[215]=true,[216]=true,[230]=true,[260]=true,[262]=true,[339]=true,[344]=true}
 if EID.isRepentance then EID.blackFeatherItems[654] = true end
