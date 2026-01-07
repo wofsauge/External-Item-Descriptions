279a25acfa45e37687bdae59b5514f4fac54f632
Xenocean <63991224+Xenocean@users.noreply.github.com>
2026-01-08 00:02:22 +0800

    Add files via upload
    corrected some translations and fixed seraphim baby not counted as a part of conjoined! in rep+ eid

--- descriptions/rep+/transformations.lua
+++ descriptions/rep+/transformations.lua
@@ -23,7 +23,7 @@ local transformations = {
 	-- Collectibles
 	["5.100.35"] = "12,16",	-- The Necronomicon: Bookworm, Necromancer
 	["5.100.262"] = "16",	-- Missing Page 2: Necromancer
-	["5.100.390"] = "10",	-- Seraphim: Seraphim
+	["5.100.390"] = "4,10",	-- Seraphim: Seraphim
 	["5.100.519"] = "4",	-- Lil Delirium: Conjoined
 	["5.350.48"] = "16",	-- A Missing Page: Necromancer
 	["5.350.71"] = "8",		-- Bobs Bladder: Bob
