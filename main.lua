3f0b611440f58e3ca3f547eb91d6f288c099b4d7
Wofsauge <jan-.-@t-online.de>
2026-01-25 00:36:06 +0100

    chore: release 5.12

--- main.lua
+++ main.lua
@@ -12,8 +12,8 @@ EID.isRepentance = REPENTANCE or EID.isRepentancePlus -- REPENTANCE variable can
 require("eid_config")
 EID.Config = EID.UserConfig
 EID.Config.Version = "3.2" -- note: changing this will reset everyone's settings to default!
-EID.ModVersion = 5.11
-EID.ModVersionCommit = "e057d2a"
+EID.ModVersion = 5.12
+EID.ModVersionCommit = "f3fa5a4"
 EID.DefaultConfig.Version = EID.Config.Version
 EID.isHidden = false
 EID.player = nil -- The primary Player Entity of Player 1
