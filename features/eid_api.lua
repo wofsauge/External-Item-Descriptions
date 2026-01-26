e5247c68c3d423f7b9275ee1d54bc680a2669850
리셰 <37092106+kohashiwakaba@users.noreply.github.com>
2026-01-26 10:49:39 +0900

    some comments

--- features/eid_api.lua
+++ features/eid_api.lua
@@ -918,7 +918,7 @@ function EID:getObjectName(Type, Variant, SubType)
 	local translatedName = EID.ItemNames[EID:getLanguage()][fallbackName]
 	local itemNameTableEntry = translatedName ~= "" and translatedName or EID.ItemNames[EID.DefaultLanguageCode][fallbackName]
 	-- return itemName table entry if found and not pills/horsepills
-	-- don't return itemName table entry if no names are found since 'EID.ItemNames[EID.DefaultLanguageCode][fallbackName]' can also be empty
+	-- 'EID.ItemNames[EID.DefaultLanguageCode][fallbackName]' can also be empty check if it is empty again, to check ItemConfig stuff propery
 	if itemNameTableEntry and itemNameTableEntry ~= "" and tableName ~= "pills" and tableName ~= "horsepills" then return itemNameTableEntry end
 
 	local tableEntry = EID:getDescriptionData(Type, Variant, SubType)
