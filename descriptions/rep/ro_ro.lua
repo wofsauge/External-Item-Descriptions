316c575fde104e69deb882105aea4c6c71f71608
f0lk-amy <falkeduardstefan@gmail.com>
2026-03-13 22:59:08 +0200

    Update ro_ro.lua

--- descriptions/rep/ro_ro.lua
+++ descriptions/rep/ro_ro.lua
@@ -484,10 +484,11 @@ EID.descriptions[languageCode].BookOfVirtuesWispTexts = {
 	-- Wisp texts are affected by the PluralizeFunction (ab+ file)
 	-- The placeholder therefore can be used in all parts that make up the wisp descriptions
 	StatDescription = "{ringIcon}{{Wisp}} {amount}|{{Heart}} {health}|{{Damage}} {damage}",	-- {ringIcon} will be replaced with an icon representing the wisp movement, {amount} with the number of wisps, {health} with the HP, {damage} with the damage or the "CantShoot" text
-	Shotspeed = "{1}% Viteza Lacrimilor", 				-- {1} will be replaced with the shot speed changes in %
+	Shotspeed = "{1}% Viteza lacrimilor", 				-- {1} will be replaced with the shot speed changes in %
+	Damage = "{1}% Daune", 					-- {1} will be replaced with the damage changes in %
 	Chance = " ({1}% Șansă)",					-- {1} will be replaced with the chance
 	SingleRoom = "Musculiță pe durata camerei",
-	NoWisps = "Fără musculițe",
+	NoWisps = "Fara musculițe",
 }
 
 -- Book of Virtues wisp types
